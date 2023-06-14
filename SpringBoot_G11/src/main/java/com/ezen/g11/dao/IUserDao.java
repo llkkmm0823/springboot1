05fe7cb53d2eb58146dd75269d40067d115cc133
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-06-14 15:53:40 +0900

    +++++++

---
+++ SpringBoot_G11/src/main/java/com/ezen/g11/dao/IUserDao.java
@@ -0,0 +1,15 @@
+package com.ezen.g11.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ezen.g11.dto.UserDto;
+
+@Mapper
+public interface IUserDao {
+	public List<UserDto> getList(); //추상메서드 생성
+	
+	
+
+}
