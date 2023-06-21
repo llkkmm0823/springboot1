19d4a1cba9373d2bef6b4c283f4752d112381530
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-06-21 17:59:35 +0900

    +++++++++++

--- SpringBoot_G15_shop/src/main/java/com/ezen/g15/dao/IAdminDao.java
+++ SpringBoot_G15_shop/src/main/java/com/ezen/g15/dao/IAdminDao.java
@@ -1,11 +1,20 @@
 package com.ezen.g15.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.ezen.g15.dto.Paging;
+import com.ezen.g15.dto.ProductVO;
 
 @Mapper
 public interface IAdminDao {
 
 	String getPwd(String workId);
+
+	int getAllCount(String string, String string2, String key);
+
+	List<ProductVO> listProduct(Paging paging, String key);
 
 	
 }
--- SpringBoot_G15_shop/src/main/java/com/ezen/g15/service/AdminService.java
+++ SpringBoot_G15_shop/src/main/java/com/ezen/g15/service/AdminService.java
@@ -1,6 +1,7 @@
 package com.ezen.g15.service;
 
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ezen.g15.dao.IAdminDao;
+import com.ezen.g15.dto.Paging;
+import com.ezen.g15.dto.ProductVO;
 
 @Service
 public class AdminService {
@@ -32,12 +35,57 @@ public class AdminService {
 	public HashMap<String, Object> getProductList(HttpServletRequest request) {
 		HashMap<String, Object> result = new HashMap<String, Object>();
 		HttpSession session = request.getSession();
+		
 		//다른 메뉴에서 현재 메뉴로 이동했을 때, 세션에 저장된 page와 key값을 모두 삭제
 		if(request.getParameter("first")!=null) {
 			session.removeAttribute("page");
 			session.removeAttribute("key");
-
 		}
+		
+		//현재 페이지 설정
+		//파라미터로 전달된 값의 유무에 따라 이동할 페이지를 결정
+		//파라미터로 전달된 페이지가 있다면 그 페이지로, 없다면 세션에 저장된 페이지로, 세션에 없다면 1페이지로 이동
+		int page=1;
+		if(request.getParameter("page")!=null) {
+			page=Integer.parseInt(request.getParameter("page"));
+			session.setAttribute("page", page);
+		}else if(session.getAttribute("page")!=null){
+			page=(int)session.getAttribute("page");
+		}else {
+			page=1;
+			session.removeAttribute("page");
+			
+		}
+		
+		//페이지와 비슷한 방식으로 검색어(key)도 설정
+		
+		String key="";
+		
+		if(request.getParameter("key")!=null) {
+			key=request.getParameter("key");
+			session.setAttribute("key", key);
+		}else if(session.getAttribute("key")!=null){
+			key=(String)session.getAttribute("key");
+		}else {
+			session.removeAttribute("key");
+			key="";
+		}
+		
+		//Paging 객체 설정
+		Paging paging = new Paging();
+		paging.setPage(page);
+		
+		int count = adao.getAllCount("product"  , "name" , key);
+		paging.setTotalCount( count );
+		paging.paging();
+		
+		List<ProductVO> productList = adao.listProduct(paging , key);
+		
+		result.put("productList", productList);
+		result.put("paging", paging);
+		result.put("key", key);
+
+		
 		return result;
 	}
 }
