d9dd7b7a9b7a6c2b739a8e9f379295d2558963c6
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-08-22 23:58:41 +0900

    Update SpringBootG01Application.java

--- SpringBoot_G01/src/main/java/com/ezen/g01/SpringBootG01Application.java
+++ SpringBoot_G01/src/main/java/com/ezen/g01/SpringBootG01Application.java
@@ -3,6 +3,7 @@ package com.ezen.g01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 스프링에 대해 다시 강의를 듣고 있는데 굉장히 오래된 코드를 학습했다는 사실을 알았다... 다시 백지로 돌아가 처음부터 다시 공부둥
 @SpringBootApplication
 public class SpringBootG01Application {
 
